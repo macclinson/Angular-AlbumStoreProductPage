115def8c1322987df3c60ffcf7f2a57a3fcbac5f
Macclinson <macclinson@gmail.com>
2019-11-08 17:12:32 +0100

    Using interface to describe data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from "./album";
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
@@ -8,7 +11,11 @@ export class ProductService {
 
   }
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map(response => response.json());
+  getAlbum(id:number):Observable<Album>{
+    return this._http
+      .get(this._albumUrl)
+      .map((response: Response) => {
+        return <Album>response.json()
+      });
   }
 }
